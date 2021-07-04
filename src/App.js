96339c3b6d4ad1491250da62aeaaa1b13fd5177a
@SoulOG <285963@supinfo.com>
2021-07-04 23:28:31 +0000

    Fin de l'implementation du formulaire

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
+import { Route } from 'react-router-dom'
 import ListContacts from "./ListContact";
 import CreateContact from "./CreateContact";
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
         state = {
-            screen: 'list',
           contacts :[]
         }
 
@@ -23,22 +23,33 @@ class App extends Component {
         ContactsAPI.remove(contact)
         }
 
+    createContact(contact){
+            ContactsAPI.create(contact).then(contact => {
+                this.setState(state => ({
+                    contacts: state.contacts.concat([ contact ])
+                }))
+            })
+    }
+
     render() {
         return (
             <div className="app">
-                {this.state.screen === 'list' && (
+                <Route exact path="/" render={() => (
                     <ListContacts
                     onDeleteContact={this.removeContact}
                     contacts={this.state.contacts}
-                    onNavigate = {() => {
-                        this.setState({screen: 'create'})
-                    }}
+                    />
+                )}
                 />
-                )}
-                {this.state.screen === 'create' && (
-                    <CreateContact/>
-                )}
-
+                <Route path="/create" render={({ history }) =>(
+                 <CreateContact
+                    onCreateContact = {(contact) => {
+                        this.createContact(contact)
+                        history.push('/')
+                    }}
+                 />
+                )
+                }/>
             </div>
         )
     }
